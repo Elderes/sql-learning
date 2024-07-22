5b4f07f503f4333dfe857c20778afa12dff36cc0
Elderes <147399524+Elderes@users.noreply.github.com>
2024-07-22 16:49:44 -0300

    Update evento-company.sql

--- SQL-Events-Company/evento-company.sql
+++ SQL-Events-Company/evento-company.sql
@@ -9,7 +9,7 @@ CREATE TABLE cliente (
 	id serial PRIMARY KEY,
   	nome varchar(70),
   	id_cliente_evento integer,
-  	CONSTRAINT cliente_evento_fk FOREIGN KEY (id_cliente_evento) REFERENCES evento(id)
+  	CONSTRAINT cliente_evento_fk FOREIGN KEY (id_cliente_evento) REFERENCES evento(id) ON DELETE set null
 );
 
 CREATE TABLE convidado (
@@ -21,9 +21,12 @@ CREATE TABLE convidado (
 
 CREATE TABLE servico (
 	id serial PRIMARY KEY,
-  	
+  	tipo varchar(100),
+  	id_servico_evento integer,
+  	CONSTRAINT servico_evento FOREIGN KEY (id_servico_evento) REFERENCES evento(id)
 );
 
 CREATE TABLE funcionario (
-
+	id serial PRIMARY KEY,
+  
 );
